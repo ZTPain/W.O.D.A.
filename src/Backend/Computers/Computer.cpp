77913bd755c6325e0696c08db0366262aaf827a7
Adam <aprzestrzelski@wp.pl>
2026-01-05 19:41:37 +0100

    fixing code style issues

--- src/Backend/Computers/Computer.cpp
+++ src/Backend/Computers/Computer.cpp
@@ -5,20 +5,28 @@
 #include "HardComputerStrategy.h"
 #include "IComputerStrategy.h"
 #include "MediumComputerStrategy.h"
+#include <stdexcept>
 
-namespace {
-EasyComputerStrategy easyStrategy;
-MediumComputerStrategy mediumStrategy;
-HardComputerStrategy hardStrategy;
-} // namespace
+static EasyComputerStrategy easyStrategy;
+static MediumComputerStrategy mediumStrategy;
+static HardComputerStrategy hardStrategy;
 
-Computer::Computer(ComputerType type)
-    : computerStrategy(
-          type == ComputerType::Medium ? static_cast<IComputerStrategy&>(mediumStrategy)
-          : type == ComputerType::Hard ? static_cast<IComputerStrategy&>(hardStrategy)
-                                       : static_cast<IComputerStrategy&>(easyStrategy)
-      ) {}
+Computer::Computer(ComputerType type) {
+  switch (type) {
+    case ComputerType::Easy:
+      computerStrategy = &easyStrategy;
+      break;
+    case ComputerType::Medium:
+      computerStrategy = &mediumStrategy;
+      break;
+    case ComputerType::Hard:
+      computerStrategy = &hardStrategy;
+      break;
+    default:
+      throw std::invalid_argument("Invalid ComputerType");
+  }
+}
 
 Coordinates Computer::GetFireCoordinates(ISegment& segmentBoard) const {
-  return computerStrategy.CalculateFireCoordinates(segmentBoard);
+  return computerStrategy->CalculateFireCoordinates(segmentBoard);
 }
