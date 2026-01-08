4f493a5996b8f3bc27e4696726c3c2744d972652
ComradeHonk <bozydar427050@gmail.com>
2026-01-08 17:09:32 +0100

    fix: add constructor, remove some stats

--- src/Backend/Games/Player.h
+++ src/Backend/Games/Player.h
@@ -3,13 +3,13 @@
 #pragma once
 
 #include "Backend/Boards/GameBoard.h"
+#include "Backend/Games/GameMode.h"
 #include "Backend/Users/UserProfile.h"
 
 struct Player {
   UserProfile& profile;
   GameBoard board;
-  unsigned int score = 0;
   unsigned int shotsFired = 0;
-  unsigned int shotsHit = 0;
-  unsigned int unitsDestroyed = 0;
+
+  Player(UserProfile& profile, const GameMode& mode);
 };
