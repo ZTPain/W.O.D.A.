764e55a0f905a4d0abe4a123d9de10136b22db0a
Sandra Dzieniszewska <sandra.inga.01@gmail.com>
2026-01-09 01:24:44 +0100

    implemented replay playback

---
+++ src/Backend/Replays/ReplayPlayback.cpp
@@ -0,0 +1,15 @@
+#include "ReplayManager.h"
+#include "Replay.h"
+#include "ReplayPlayback.h"
+
+ReplayManager ReplayManager::instance;
+
+ReplayManager::ReplayManager() = default;
+ReplayManager& ReplayManager::GetInstance() { return instance; }
+ReplayManager::~ReplayManager() = default;
+const std::vector<Replay>& ReplayManager::Replays() const { return replays; }
+void ReplayManager::SaveReplay(const Replay& replay) { replays.push_back(replay); }
+ReplayPlayback ReplayManager::Play(unsigned int replayId) {
+  return
+        {replays[replayId]};
+}
