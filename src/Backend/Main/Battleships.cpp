23ec3c7169d57098f4d2d6ac9ae364e0451f6610
Wydział Wewnętrzny <35401291+WW5833@users.noreply.github.com>
2026-01-14 18:33:39 +0100

    Misc updates to Battleships

--- src/Backend/Main/Battleships.cpp
+++ src/Backend/Main/Battleships.cpp
@@ -104,17 +104,17 @@ void Battleships::ReadSave() {
 }
 
 void Battleships::WriteToSave() const {
-  std::ofstream file("save.dat", std::ios::binary);
+  std::ofstream file("save.dat", std::ios::binary | std::ios::trunc);
 
   if (!file)
     return;
 
   const auto& users = userManager.Users();
 
-  // 10 MB should be enough
-  constexpr auto BUFFER_SIZE = static_cast<const size_t>(10 * 1024 * 1024);
+  // 1 MB should be enough
+  constexpr auto BUFFER_SIZE = static_cast<const size_t>(1 * 1024 * 1024);
 
-  std::array<uint8_t, BUFFER_SIZE> buffer{};
+  static std::array<uint8_t, BUFFER_SIZE> buffer{};
   size_t offset = 0;
 
   SerializationHelper::SerializeInt32(buffer.data(), offset, BUFFER_SIZE, users.size());
