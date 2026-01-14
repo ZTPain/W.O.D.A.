9e12405c5bb43e0fdb5175e3a6ec16313b2f0d0d
Wydział Wewnętrzny <35401291+WW5833@users.noreply.github.com>
2026-01-14 18:30:13 +0100

    Add some getters to GameManager

--- src/Backend/Games/GameManager.cpp
+++ src/Backend/Games/GameManager.cpp
@@ -29,6 +29,12 @@ unsigned int GameManager::CurrentTurn() const { return currentTurn; }
 
 Player& GameManager::GetCurrentPlayer() { return players[currentTurn]; }
 
+GameState GameManager::State() const { return state; }
+
+unsigned int GameManager::WinnerId() const { return winnerId; }
+
+std::chrono::seconds GameManager::Playtime() const { return playtime; }
+
 void GameManager::StartGame() {
   gameStartPoint = std::chrono::steady_clock::now();
   state = GameState::Playing;
